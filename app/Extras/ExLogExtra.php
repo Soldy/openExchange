0b547ed44beefe12d506d1181e297adfaaec48cd
Soldy <4786022+Soldy@users.noreply.github.com>
2020-02-12 23:56:26 +0000

    add log for stat
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ app/Extras/ExLogExtra.php
@@ -0,0 +1,25 @@
+
+<?php
+
+namespace App\Traits;
+
+use App\ExLog;
+
+
+class ExLogExtras
+{
+    public function add ($to="",$form="",$amount=""){
+        if($to=="")
+            return false;
+        if($form=="")
+            return false;
+        if($amount=="")
+            return false;
+        $log = new ExLog();
+        $log->to=$to;
+        $log->form=$form;
+        $log->amount=$amount;
+        $log->save();
+        return $log->id;
+    }
+}
